85151ce4b7ab1fe5c5f7a471c669515a27ee5959
Franz Helmberger <Franz Helmberger>
2016-05-01 21:09:49 +0200

    ts  style

--- HttpTriggerNodeJS1/index.js
+++ HttpTriggerNodeJS1/index.js
@@ -1,17 +1,49 @@
-module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+"use strict";
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+module.exports = A.myfunction;
+
+var A = (function () {
+    function A() {
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
-};
+    A.myfunction = function (context, req) {
+        context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+        if (req.query.name || (req.body && req.body.name)) {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: "Hello ABC " + (req.query.name || req.body.name)
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "Please pass a name on the query string or in the request body"
+            };
+        }
+        context.done();
+    };
+    ;
+    return A;
+}());
+exports.A = A;
+
+
+//# sourceMappingURL=index.js.map
+
+
+// module.exports = function(context, req) {
+//     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+
+//     if (req.query.name || (req.body && req.body.name)) {
+//         context.res = {
+//             // status: 200, /* Defaults to 200 */
+//             body: "Hello " + (req.query.name || req.body.name)
+//         };
+//     }
+//     else {
+//         context.res = {
+//             status: 400,
+//             body: "Please pass a name on the query string or in the request body"
+//         };
+//     }
+//     context.done();
+// };
