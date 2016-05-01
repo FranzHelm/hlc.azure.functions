936dd1c0d21419d5c4e27d2e50deee18c4bb6135
Franz Helmberger <Franz Helmberger>
2016-05-01 21:16:38 +0200

    ts4

--- HttpTriggerNodeJS1/index.js
+++ HttpTriggerNodeJS1/index.js
@@ -1,4 +1,4 @@
-
+"use strict";
 var A = (function () {
     function A() {
     }
@@ -22,28 +22,5 @@ var A = (function () {
     return A;
 }());
 exports.A = A;
-
-
 module.exports = A.myfunction;
-
-
 //# sourceMappingURL=index.js.map
-
-
-// module.exports = function(context, req) {
-//     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-
-//     if (req.query.name || (req.body && req.body.name)) {
-//         context.res = {
-//             // status: 200, /* Defaults to 200 */
-//             body: "Hello " + (req.query.name || req.body.name)
-//         };
-//     }
-//     else {
-//         context.res = {
-//             status: 400,
-//             body: "Please pass a name on the query string or in the request body"
-//         };
-//     }
-//     context.done();
-// };
