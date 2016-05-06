e2d88693ac46e05266affb0ea55b7d8208e5da68
Franz Helmberger <Franz Helmberger>
2016-05-06 21:45:40 +0200

    Copy HttpTriggerNodeJS1 to 2

---
+++ HttpTriggerNodeJS2/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var keepalive_1 = require("./tasks/keepalive");
+var A = (function () {
+    function A() {
+    }
+    A.myfunction = function (context, req) {
+        context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+        var keepAlive = new keepalive_1.KeepAlive();
+        keepAlive.setContext(context);
+        keepAlive.setArguments(arguments);
+        context.res = keepAlive.ping(req);
+        // if (req.query.name || (req.body && req.body.name)) {
+        //     context.res = {
+        //         // status: 200, /* Defaults to 200 */
+        //         body: "Hello ABC " + (req.query.name || req.body.name)
+        //     };
+        // }
+        // else {
+        //     context.res = {
+        //         status: 400,
+        //         body: "Please pass a name on the query string or in the request body"
+        //     };
+        // }
+        context.done();
+    };
+    ;
+    return A;
+}());
+exports.A = A;
+module.exports = A.myfunction;
+//# sourceMappingURL=index.js.map
